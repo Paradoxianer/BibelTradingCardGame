37aca2f76e0e8c68a9d6c732a113b247ae835369
Paradoxon <two4god@gmail.com>
2016-08-22 18:04:35 +0200

    Added GameBoard Class wich repesent the Area of the GameTable where you play in

---
+++ Source/header/gamedefs.h
@@ -0,0 +1,4 @@
+#ifndef GAMEDEFS_H
+#define GAMEDEFS_H
+
+#endif // GAMEDEFS_H
--- strength.cpp
+++
@@ -1,8 +0,0 @@
-#include "strength.h"
-
-Strength::Strength(int  howMuch,QPoint there,float howBig)
-{
-    strenght    = howMuch;
-    size        = howBig;
-    where       = there;
-}
